e39a8b49042253cdb58e4dbb185d481c0cc0a787
ahmadnurmuarif290204 <136594254+ahmadnurmuarif290204@users.noreply.github.com>
2025-01-14 10:20:54 +0700

    Update pada api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Database\Eloquent\Model;
 use App\Http\Controllers\QuranController;
 
 
-// untuk api surah
+// Untuk mengambil API surah
 Route::get('/quran', [QuranController::class, 'quran']);
-// untuk api ayat
+
+// Untuk mengambil API detail ayat
+Route::get('/details', [QuranController::class, 'getAllDetails']);
 Route::get('/details/{nomorSurah}', [QuranController::class, 'getDetailsBySurah']);
-Route::get('/details', [QuranController::class, 'getAllDetail']);
 Route::get('/details/{nomorSurah}/{nomorAyat}', [QuranController::class, 'getDetailAyat']);
-Route::get('/import-details/{nomorSurah}', [QuranController::class, 'importDetailsBySurah']);
-// untuk api tafsir nya 
-Route::get('/tafsir/{nomorSurah}', [QuranController::class, 'getTafsirBySurah']);
+Route::get('/import-details', [QuranController::class, 'importDetails']);
+
+// Untuk mengambil API tafsir
 Route::get('/tafsir', [QuranController::class, 'getAllTafsir']);
+Route::get('/tafsir/{nomorSurah}', [QuranController::class, 'getTafsirsBySurah']);
 Route::get('/tafsir/{nomorSurah}/{nomorAyat}', [QuranController::class, 'getTafsirByAyat']);
-Route::get('/import-tafsir/{nomorSurah}', [QuranController::class, 'importTafsirBySurah']);
+Route::get('/import-tafsir', [QuranController::class, 'importTafsir']);
